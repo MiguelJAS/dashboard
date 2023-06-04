e056c8c5bcdf968daea919464aa1efc46e3b71cf
Miguel Jesus <miguel.alfonsosanchez9@gmail.com>
2023-06-04 20:30:38 +0200

    dashboard a medias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import UserList from "./componentes/DashBoard/UserList/UserList";
+import RegisterForm from "./componentes/RegisterForm/RegisterForm";
+import Menu from "./componentes/DashBoard/Menu/Menu";
+import useUsers from "./hooks/useUsers";
 
-function App() {
+const App = () => {
+  const {
+    users,
+    userUpdate,
+    initialUserRegister,
+    visibleForm,
+    handleUpdateUser,
+    handlerAddUser,
+    handlerRemoveUser,
+    handleOpenForm,
+    handleCloseForm,
+  } = useUsers();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex columns-2">
+        <Menu></Menu>
+        <div className="md:container md:mx-auto">
+          <div className="">
+            {users.length === 0 ? (
+              <div
+                className="mt-36 flex items-center justify-center border-l-4 border-orange-500 bg-orange-100 p-4 text-orange-700"
+                role="alert"
+              >
+                <p>No hay usuarios en el sistema!</p>
+              </div>
+            ) : (
+              <UserList
+                users={users}
+                handlerRemoveUser={handlerRemoveUser}
+                handleUpdateUser={handleUpdateUser}
+                handleOpenForm={handleOpenForm}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+
+      {!visibleForm || (
+        <div className="mt-48">
+          <RegisterForm
+            handlerAddUser={handlerAddUser}
+            initialUserRegister={initialUserRegister}
+            userUpdate={userUpdate}
+            handleCloseForm={handleCloseForm}
+          />
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/componentes/DashBoard/UserList/UserList.js
@@ -0,0 +1,55 @@
+import BotonCrearUsuario from "../../Buttons/BotonCrearUsuario";
+import UserRow from "../UserRow/UserRow";
+
+const UserList = ({
+  users = [],
+  handlerRemoveUser,
+  handleUpdateUser,
+  handleOpenForm,
+}) => {
+  return (
+    <>
+      <h1 className="mb-14 mt-10 text-center font-mono text-3xl font-semibold text-indigo-600">
+        Lista de Usuarios
+      </h1>
+      <BotonCrearUsuario handleOpenForm={handleOpenForm} />
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left rtl:text-right">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                #
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Nombre
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Apellidos
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Email
+              </th>
+              <th className="px-4 py-2" />
+            </tr>
+          </thead>
+
+          <tbody class="divide-y divide-gray-200">
+            {users.map(({ id, firstName, surname, email }) => (
+              <UserRow
+                key={id}
+                id={id}
+                firstName={firstName}
+                surname={surname}
+                email={email}
+                handlerRemoveUser={handlerRemoveUser}
+                handleUpdateUser={handleUpdateUser}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default UserList;
